89928cf23a1958f88556e1f9d188854695e08e32
arjunachala <arjuna.chala@lexisnexis.com>
2012-12-10 11:27:37 -0500

    First Commit

--- mod-hpcc-sql/src/org/hpccsystems/sql/Parser.java
+++ mod-hpcc-sql/src/org/hpccsystems/sql/Parser.java
@@ -1,6 +1,7 @@
 package org.hpccsystems.sql;
 
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.select.Join;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 
@@ -23,18 +24,32 @@ public final class Parser {
         Select body = (Select) parserManager.parse(new StringReader(sql));
         PlainSelect select = (PlainSelect) body.getSelectBody();
 
-        if (select.getFromItem() instanceof  net.sf.jsqlparser.schema.Table)  {
-           Table table = new Table();
+        if (select.getFromItem() instanceof net.sf.jsqlparser.schema.Table) {
+            Table table = new Table();
 
             net.sf.jsqlparser.schema.Table jsqlTbl = (net.sf.jsqlparser.schema.Table) select.getFromItem();
             table.setName(jsqlTbl.getName());
             table.setAlias(jsqlTbl.getAlias());
             table.setSchema(jsqlTbl.getSchemaName());
+
+            FromItem fromItem = new FromItem();
+            fromItem.setLeftTable(table);
+
+            if (select.getJoins() != null && select.getJoins().size() > 0) {
+                Table rightTable = new Table();
+                Join join  = (Join) select.getJoins().get(0);
+                net.sf.jsqlparser.schema.Table rightJsqlTbl = (net.sf.jsqlparser.schema.Table) join.getRightItem();
+                rightTable.setName(rightJsqlTbl.getName());
+                rightTable.setAlias(rightJsqlTbl.getAlias());
+                rightTable.setSchema(rightJsqlTbl.getSchemaName());
+
+                fromItem.setRightTable(table);
+            }
         }
 
     }
 
-    public  List<FromItem> getFromItemList() {
+    public List<FromItem> getFromItemList() {
         return fromItemList;
     }
 
